fb630aa86c2b25218cf264ca71363d5c645d289a
michaelnaps <napoli.38@osu.edu>
2021-08-08 11:40:36 -0400

    computer guess skipping numbers

--- tutorial/t01/hw01.lua
+++ tutorial/t01/hw01.lua
@@ -2,52 +2,57 @@
 -- Created by: Michael Napoli
 -- Created on: 7/20/2021
 
--- test function syntax for simple example
-function test_func(x, y)
-   return x * y
-end
-
--- test case
--- print(test_func(2, 3))
-
+math.randomseed(os.time())
 
 -- Problem 1
-function user_guess(val)
-   print("First guess: ")
-   guess = io.read("*n")
+function user_guess()
+  val = math.random(100)
 
-   while val ~= guess do
-      print("New guess: ")
-      guess = io.read("*n")
+  print("First guess: ")
+  guess = io.read("*n")
 
-      if guess > val then
-         print("Too high.")
-      elseif guess < val then
-         print("Too low.")
-      end
+  while val ~= guess do
+    if guess > val then
+      print("Too high.")
+    elseif guess < val then
+      print("Too low.")
+    end
 
-      print("Correct!")
-   end
+    print("New guess: ")
+    guess = tonumber(io.read("*n"))
+  end
+
+  print("Correct!")
 end
 
 -- test case
-user_guess(10)
+-- user_guess()
 
 
 -- Problem 2
 function comp_guess()
-   guess = 50
-   while input ~= 'C' do
-      print("Guess:", guess)
-      print("Is the guess correct?")
-      input = io.read(1)
+  count = 1
+  l = 0
+  h = 100
+  guess = 50
 
-      if input == "H" then
-         guess = guess / 2
-      elseif input == "L" then
-         guess = guess + 5
-      end
-   end
+  while input ~= 'C' and input ~= 'c' do
+    print("Guess:", guess)
+
+    print("Is the guess correct?")
+    input = io.read(1, "*l")
+
+    if input == 'L' or input == 'l' then
+      l = guess
+    elseif input == 'H' or input == 'h' then
+      h = guess
+    end
+
+    guess = math.floor((l + h) / 2)
+    count = count + 1
+  end
+
+print("Correct! Guesses needed: ", count)
 end
 
 -- test case
